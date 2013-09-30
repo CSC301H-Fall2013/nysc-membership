40f1e6377c3a2812fcae37776ba83ecd08e513d2
codywong <cody.wong@utoronto.ca>
2013-09-30 18:16:38 -0400

    adding it again!

---
+++ nysc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nysc::Application.config.secret_token = '1eb543a0135f87b6e8120245262d42af548402d8bfe6c0a95d74e943e3819e9ba66a377753fcd62d24226981773f72bf031fecd2c7ea1f12bb1e041a43f5f368'
