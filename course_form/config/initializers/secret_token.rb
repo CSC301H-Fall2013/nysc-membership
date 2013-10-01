8ccd1af622407b5667e50830eb46acd38ceca269
Jana <jana.stc>
2013-10-01 17:46:55 -0400

    Added the folder course_forms

---
+++ course_form/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CourseForm::Application.config.secret_key_base = '524acceb1fa5e37241e6366d850cdf187648ba08cd147182ee5052d098590661318ba856c5aaa5d2d15aeb75edeeb04c6be30b136610348be6696c77200870ec'
