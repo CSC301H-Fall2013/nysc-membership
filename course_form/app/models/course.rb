10a3e1214408dd08083251e7a500c062d4b63758
eshmoel <eshmoel@gmail.com>
2013-10-02 18:55:01 -0400

    more fixes - also fixed duplicate course codes

--- course_form/app/models/course.rb
+++ course_form/app/models/course.rb
@@ -1,10 +1,11 @@
 class Course < ActiveRecord::Base
 
 
-	validates :courseName, :courseCode, :instructor, :description, :intensity, :additional, :duration, :earlybirdPrice, :presence => true
+	validates :courseName, :courseCode, :instructor, :description, :intensity, :duration, :earlybirdPrice, :presence => true
 	validates :startDate, :endDate, :presence => { :message => "Date must be in DD-MM-YYYY"}
 	validates :startTime, :endTime, :presence => { :message => "Time must be in HH:MM"}
 	validates :memberPrice, :nonmemberPrice, :presence => true
+	validates :courseCode, :uniqueness => { :message => ": This course code already exists!"}
 
 
 
