e7cb45f01a9dd1d122811b6e7d9b7b8b07533057
jessiluu <jessica.p.y.luu@gmail.com>
2013-10-02 18:31:40 -0400

    Update course.rb
    Edited variable names

--- course_form/app/models/course.rb
+++ course_form/app/models/course.rb
@@ -4,7 +4,7 @@ class Course < ActiveRecord::Base
 	validates :courseName, :courseCode, :instructor, :description, :intensity, :additional, :duration, :earlybirdPrice, :presence => true
 	validates :startDate, :endDate, :presence => { :message => "Date must be in DD-MM-YYYY"}
 	validates :startTime, :endTime, :presence => { :message => "Time must be in HH:MM"}
-	#validates :memberPrice, :nonmemberPrice, :presence => true
+	validates :membersPrice, :nonmembersPrice, :presence => true
 
 
 
