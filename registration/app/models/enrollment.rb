03a1d0cde2e28aba2b201c856b9945cb14d51f67
Michelle Liao <mich.liao@mail.utoronto.ca>
2013-10-21 21:34:12 -0400

    implemented user story 10

--- registration/app/models/enrollment.rb
+++ registration/app/models/enrollment.rb
@@ -1,5 +1,17 @@
 class Enrollment < ActiveRecord::Base
 
+
+	# Search for all participants that match in the database
+	# by participantID, courseID, or startDate
+	def self.search(search)
+	  if search
+	    Enrollment.where(['participantID LIKE ? OR courseID LIKE ? OR startDate LIKE ? ', "%#{search}%", "%#{search}%", "%#{search}%"])
+	  else # When page is initially loaded display no registeration information.
+	    []
+	  end
+	end
+
+	#Validation
 	validates :participantID, :uniqueness => { :scope => :courseID }
 
 	validates :participantID, :courseID, :startDate, :waitlist_status, :presence => true
