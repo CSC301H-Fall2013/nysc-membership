cc0ba2b0ac459c3cfae6970ab01bfc70de9eab36
Jana <jana.stc>
2013-10-22 17:39:10 -0400

    Added validation for startTime and EndTime conflicts for course

--- registration/app/models/course.rb
+++ registration/app/models/course.rb
@@ -7,5 +7,15 @@ class Course < ActiveRecord::Base
 	 :duration, :startTime, :endTime, :dayOfWeek, :earlybirdPrice, :memberPrice, :nonmemberPrice, \
 	  :size, :presence => true
 
-	 validates :startDate, :presence => { :message => "Date must be in DD-MM-YYYY"}
+	validates :startDate, :presence => { :message => "Date must be in DD-MM-YYYY"}
+
+	validate :startTime_endTime_conflict
+
+
+	def startTime_endTime_conflict
+		if :startTime >= :endTime
+			errors.add(:course, "End Time Ahead of Start Time")
+		end
+	end
+
 end
