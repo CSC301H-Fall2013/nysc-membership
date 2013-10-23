5df91ed53f9e025a011221f41ce666386bd4364e
Jana <jana.stc>
2013-10-22 21:52:06 -0400

    Added a new page to list participants in a class: User story 20

--- registration/config/routes.rb
+++ registration/config/routes.rb
@@ -10,6 +10,8 @@ Registration::Application.routes.draw do
 
   resources :admins
 
+  get "static_pages/listParticipants"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
