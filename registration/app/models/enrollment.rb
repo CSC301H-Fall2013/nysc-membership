602d837412f115ab358c5e3befdaf3c412cb309e
Michelle Liao <mich.liao@mail.utoronto.ca>
2013-11-12 02:31:09 -0500

    appropriate fees added

--- registration/app/models/enrollment.rb
+++ registration/app/models/enrollment.rb
@@ -37,6 +37,47 @@ class Enrollment < ActiveRecord::Base
 		end
 	end
 
+	#Check if enrollment ID inputs are in the database
+	def self.check_validation(enrollment)
+	    if Participant.find_by(participantID: enrollment.participantID) 
+	    	if Course.find_by(courseID: enrollment.courseID) 
+	      		return true
+	    	end
+	    end
+	end
+
+	#Charges participant fee by their membership and date
+	def self.charge_fee(enrollment)
+		if_member = Participant.find_by(participantID: enrollment.participantID)
+		if_course = Course.find_by(courseID: enrollment.courseID)
+		earlyBirdTime = if_course.startDate.months_ago(1)
+		if if_member.expirydate > Date.today
+			if Date.today >= earlyBirdTime && Date.today < if_course.startDate
+				return if_course.earlybirdPrice
+			else
+				return if_course.memberPrice
+			end
+		else
+			return if_course.nonmemberPrice
+		end
+	end
+
+
+	def self.get_season #should change the years later
+		if Date.today.month >= 1 && Date.today.month <= 3
+			return Date.parse("2013-01-01")
+		end
+		if Date.today.month >= 4 && Date.today.month <= 6
+			return Date.parse("2013-04-01")
+		end
+		if Date.today.month >= 7 && Date.today.month <= 9
+			return Date.parse("2013-07-01")
+		else 
+			return Date.parse("2013-10-01")
+		end
+	end
+
+
 	# # when user clicks the "validate" button beside the courseID input field with a courseID, create the pop-up
 	# # displaying if participant will be waitlisted, and how much they will need to pay, either now or later
 	# def self.gen_popup_validate(courseID)
@@ -50,10 +91,15 @@ class Enrollment < ActiveRecord::Base
 	# end
 
 	#Validation
-	validates :participantID, :uniqueness => { :scope => :courseID }
+
+	#Primary key of courseID and participantID
+	validates :participantID, :presence => true, :uniqueness => {:scope => :courseID, :message => "has been already enrolled in that course"}
+	validates :courseID, :presence => true, :uniqueness => {:scope => :participantID, :message => "has that participant enrolled already"}
+	
+	validates :courseID, length: { is: 8 }
 	validate :course_exists
 	validate :course_full
-	validates :participantID, :courseID, :startDate, :waitlist_status, :presence => true
+	validates :participantID, :courseID, :startDate, :presence => true
 	validates :startDate, :presence => { :message => "Date must be in DD-MM-YYYY"}
 
 
