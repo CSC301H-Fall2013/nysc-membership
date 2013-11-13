0a697b136c9189164ac542793846eb110cf20087
jessiluu <jessica.p.y.luu@gmail.com>
2013-11-12 22:47:37 -0500

    waitlist

---
+++ registration/app/controllers/waitlist_controller.rb
@@ -0,0 +1,24 @@
+class WaitlistController < ApplicationController
+	before_action :set_waitlist, only: [:show, :edit, :update, :destroy]
+  # GET /waitlist
+  # GET /waitlist.json
+  def index
+    @courses = Course.all
+  end
+  
+  # GET /waitlist/1
+  # GET /waitlist/1.json
+  def show
+  end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_waitlist
+      @course = Course.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def course_params
+      params.require(:course).permit(:courseID, :startDate, :title, :instructor, :description, :intesity, :additional, :duration, :startTime, :endTime, :dayOfWeek, :earlybirdPrice, :memberPrice, :nonmemberPrice, :size)
+    end  
+end
