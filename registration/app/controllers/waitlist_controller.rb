438f0fcf3f5063481ef531dd922024ccea9784bc
jessiluu <jessica.p.y.luu@gmail.com>
2013-11-13 01:03:19 -0500

    streamline format and rid of unnecessary file

--- registration/app/controllers/waitlist_controller.rb
+++ registration/app/controllers/waitlist_controller.rb
@@ -11,6 +11,29 @@ class WaitlistController < ApplicationController
       @enrollments = Enrollment.where(:courseID => @course.courseID, :startDate => @course.startDate).where(['enrollments.waitlist_status <> ?', 0])
   end
   
+  # GET /waitlist/new
+  def new
+  end
+  
+  # GET /waitlist/1/edit
+  def edit
+  end
+  
+  # POST /waitlist
+  # POST /waitlist.json
+  def create
+  end
+  
+  # PATCH/PUT /waitlist/1
+  # PATCH/PUT /waitlist/1.json
+  def update
+  end
+  
+  # DELETE /waitlist/1
+  # DELETE /waitlist/1.json
+  def destroy
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_waitlist
