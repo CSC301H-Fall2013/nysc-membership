d83c34c29644988845fe5bb74d3cd034d6ec516e
jana-uoft <jana.stc@gmail.com>
2013-11-27 05:33:15 +0000

    deleted payment notification controller

--- registration/app/controllers/payment_notification_controller.rb
+++
@@ -1,9 +0,0 @@
-class PaymentNotificationController < ApplicationController
-    protect_from_forgery :except => [:create]
-  
-  def create
-    PaymentNotification.create!(:params => params, :cart_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:tx])
-  end
-  
-  
-end
--- registration/test/helpers/payment_notification_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PaymentNotificationHelperTest < ActionView::TestCase
-end
