87ecb3aecc6fc499388bfc0b90bcf119b21885c9
jessiluu <jessica.p.y.luu@gmail.com>
2013-11-27 20:21:08 -0500

    Modified payment notification controller to enroll

--- registration/app/controllers/paymentnotification_controller.rb
+++ registration/app/controllers/paymentnotification_controller.rb
@@ -6,17 +6,18 @@ class PaymentnotificationController < ApplicationController
     @notification.transaction_id = params[:ipn_track_id]
     @notification.params = params
     @notification.status = "paid"
-    @custom = Base64.decode64(params[:custom])
-    @custom = @custom.split("|")
-    @user = User.new
-    @user.email = @custom[0]
-    @user.organization_type_id = @custom[1].to_i
-    @user.password = @custom[2]
-    if @user.valid?
-      @user.save
-      @notification.user = @user
+    #@custom = Base64.decode64(params[:custom])
+    #@custom = @custom.split("|")
+    #@user = User.new
+    #@user.email = @custom[0]
+    #@user.organization_type_id = @custom[1].to_i
+    #@user.password = @custom[2]
+    @enrollment = session[:enroll_save]
+    if @enrollment.participantID == current_participant.participantID
+      @enrollment.save
+      #@notification.user = @user
       @notification.save
-      @user.send_confirmation_instructions
+      #@user.send_confirmation_instructions
     end
     render :nothing => true
   end
