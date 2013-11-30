b4d6a92aa19984c7e5ab820bcbfa1e1c31ea97b9
Michelle Liao <mich.liao@mail.utoronto.ca>
2013-11-30 10:29:36 -0500

    participant page changed

--- registration/app/controllers/static_pages_controller.rb
+++ registration/app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def participanthome
+  	@course = Enrollment.where(:participantID => current_participant.participantID)
   end
+
+  def check_participant
+    @participant = Participant.find_by_id(params[:id])
+    # Then you can access the teachers with @school.teachers
+  end
+
 end
